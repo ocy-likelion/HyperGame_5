ad28aeb1a2e2e7aa0602024f67f28d3c2b18ea66
hipop1109 <hipop1109@gmail.com>
2025-08-14 16:17:27 +0900

    [FIX] Merge Bug Fix
    머지 관련 버그 수정

--- Assets/1_Append/LHS/GameManager.cs
+++ Assets/1_Append/LHS/GameManager.cs
@@ -11,6 +11,14 @@ public class GameManager : MonoBehaviour
     public Image GameOverPanel;
     public TMP_Text CountdownText; // unused
 
+    public UIManager uiManager; // UI 매니저 참조
+    private bool gameEnd = false; // 게임 종료 여부
+    public bool isWin = false; // 게임 승리 여부
+    public float timerDuration = 30f; // 제한 시간
+    private float currentTime;
+    public int score = 0; // 게임 점수;
+
+
     void OnEnable()
     {
         EventBus.Instance.Subscribe(Consts.GAMECLEAR, GameClear);
@@ -25,13 +33,38 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        currentTime = timerDuration; // 시작 시 타이머 초기화
 
     }
 
     void Update()
     {
+        if (gameEnd) return; // 이미 종료 상태면 더 이상 진행 안 함
 
+        // 타이머 감소
+        currentTime -= Time.deltaTime;
+
+        // 조건 체크
+        if (currentTime <= 0 || isWin)
+        {
+            gameEnd = true;
+            EndGame();
+        }
     }
+
+    private void EndGame()
+    {
+        // 결과 UI 활성화
+        uiManager.ShowResultUI();
+        uiManager.Result(isWin);
+    }
+
+    // 테스트용 승리 트리거
+    public void Test()
+    {
+        isWin = true;
+    }
+
 
     #region 게임 진행
 
