e05dab8e7a2d10719f9fa02bd8b7c15df5a6a329
hipop1109 <hipop1109@gmail.com>
2025-08-14 16:42:40 +0900

    [FIX] Merge Fix
    머지 부분 수정 및 합침

--- Assets/1_Append/LHS/GameManager.cs
+++ Assets/1_Append/LHS/GameManager.cs
@@ -5,12 +5,6 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public Button DropButton; // unused
-    public Image ClearPanel;
-    public Image PausePanel;
-    public Image GameOverPanel;
-    public TMP_Text CountdownText; // unused
-
     public UIManager uiManager; // UI 매니저 참조
     private bool gameEnd = false; // 게임 종료 여부
     public bool isWin = false; // 게임 승리 여부
@@ -21,14 +15,14 @@ public class GameManager : MonoBehaviour
 
     void OnEnable()
     {
-        EventBus.Instance.Subscribe(Consts.GAMECLEAR, GameClear);
-        EventBus.Instance.Subscribe(Consts.GAMEOVER, GameOver);
+        //EventBus.Instance.Subscribe(Consts.GAMECLEAR, GameClear);
+       // EventBus.Instance.Subscribe(Consts.GAMEOVER, GameOver);
     }
 
     void OnDisable()
     {
-        EventBus.Instance.Unsubscribe(Consts.GAMECLEAR, GameClear);
-        EventBus.Instance.Unsubscribe(Consts.GAMEOVER, GameOver);
+       // EventBus.Instance.Unsubscribe(Consts.GAMECLEAR, GameClear);
+       // EventBus.Instance.Unsubscribe(Consts.GAMEOVER, GameOver);
     }
 
     void Start()
@@ -65,57 +59,6 @@ public class GameManager : MonoBehaviour
         isWin = true;
     }
 
-
-    #region 게임 진행
-
-    public void StartGame()
-    {
-        // 게임 시작
-    }
-
-    public void PauseGame()
-    {
-        // 게임 일시정지
-        Time.timeScale = 0.0f;
-        PausePanel.gameObject.SetActive(true);
-    }
-
-    public void ResumeGame()
-    {
-        // 게임 재개
-        Time.timeScale = 1.0f;
-        PausePanel.gameObject.SetActive(false);
-    }
-
-    public void RestartGame()
-    {
-        // 게임 재시작
-        Time.timeScale = 1.0f;
-        ClearPanel.gameObject.SetActive(false);
-        GameOverPanel.gameObject.SetActive(false);
-        SceneManager.LoadScene(Consts.GAMESCENE);
-    }
-
-    public void QuitGame()
-    {
-        // 메인 메뉴로?
-        //SceneManager.LoadScene();
-    }
-
-    void GameClear()
-    {
-        // 게임 클리어
-        Time.timeScale = 0.0f;
-        ClearPanel.gameObject.SetActive(true);
-    }
-
-    void GameOver()
-    {
-        // 게임 오버
-        Time.timeScale = 0.0f;
-        GameOverPanel.gameObject.SetActive(true);
-    }
-    #endregion
 
     #region 소리
     void SoundOn()
