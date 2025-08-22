2109631f8b8413dc7290c3ad67baa4c52ca4ddc3
NoRhozhoroski <137853317+RhoGyeongwon@users.noreply.github.com>
2025-08-22 13:56:16 +0900

    Fix : 블럭 자주 무너지는 버그 수정

--- Assets/1_Append/Rho/1.Script/BlockOnlyTop.cs
+++ Assets/1_Append/Rho/1.Script/BlockOnlyTop.cs
@@ -1,9 +1,12 @@
+using DG.Tweening;
+using System.Collections;
 using UnityEngine;
 
 public class BlockOnlyTop : MonoBehaviour
 {
     private Rigidbody2D rb;
     private readonly float slideForce = -7f;
+    private Coroutine coroutine;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -21,4 +24,31 @@ public class BlockOnlyTop : MonoBehaviour
     {
         rb.linearVelocity = new Vector2(slideForce, slideForce);
     }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Block") || collision.gameObject.CompareTag("Platform"))
+        {
+            if (coroutine != null)
+            {
+                StopCoroutine(coroutine);
+            }
+
+            coroutine = StartCoroutine(TopBlockSettingCoroutine());
+        }
+    }
+
+    IEnumerator TopBlockSettingCoroutine()
+    {
+        Rigidbody2D rb = this.gameObject.GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Kinematic;
+
+        yield return new WaitForSeconds(0.1f);
+
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        rb.gravityScale = 1f;
+        rb.linearVelocity = Vector2.zero;
+        rb.angularVelocity = 0f;
+    }
+
 }
