17613f1a3bb47d792cc2a0368dac33dfc8edc1cd
[ESGUnity] <[sshsong3908@naver.com]>
2025-08-29 15:21:02 +0900

    feat : 블럭이 떨어질 때 타임스케일 조절(제거 가능)

--- Assets/1_Append/SSH/01_Scripts/DamagePostEffect.cs
+++ Assets/1_Append/SSH/01_Scripts/DamagePostEffect.cs
@@ -7,8 +7,12 @@ public class DamagePostEffect : MonoBehaviour
 {
     // 상수
     private const float MAX_INTENSITY = 0.5f; // 최대 강도
-    private const float FADE_IN_TIME = 0.1f;     // 깜빡일 때 올라가는 시간
-    private const float FADE_OUT_TIME = 0.5f;    // 서서히 사라지는 시간
+    private const float FADE_IN_TIME = 0.1f;  // 깜빡일 때 올라가는 시간
+    private const float FADE_OUT_TIME = 0.5f; // 서서히 사라지는 시간
+
+    private const float SLOW_TIME_SCALE = 0.5f; // 피격 시 슬로우 모션 타임스케일
+    private const float SLOW_DURATION = 0.2f;   // 슬로우 유지 시간
+    private const float TIME_RECOVER_DURATION = 0.3f; // 원래 속도로 복귀하는 시간
 
     // private 필드(인스펙터 노출)
     [SerializeField] private Volume postProcessVolume;
@@ -41,23 +45,23 @@ public class DamagePostEffect : MonoBehaviour
         }
         else
         {
-            Debug.Log("Vignette 없음");
+            Debug.LogError("Vignette 없음");
         }
     }
 
+    // 메인
     public void PlayDamageEffect()
     {
         if (vignette == null)
         {
-            Debug.Log("Vignette 없음");
-
+            Debug.LogError("Vignette 없음");
             return;
         }
 
         DOTween.Kill(this); // 중복 방지
         vignette.intensity.value = 0f;
 
-        // 피격 연출
+        // Vignette 연출
         DOTween.To(() => vignette.intensity.value,
                    x => vignette.intensity.value = x,
                    MAX_INTENSITY,
@@ -68,6 +72,23 @@ public class DamagePostEffect : MonoBehaviour
                               x => vignette.intensity.value = x,
                               0f,
                               FADE_OUT_TIME);
+               });
+
+        // 타임스케일 연출
+        DOTween.To(() => Time.timeScale,
+                   x => Time.timeScale = x,
+                   SLOW_TIME_SCALE,
+                   0.05f) // 빠르게 느려짐
+               .OnComplete(() =>
+               {
+                   // 일정 시간 유지 후 원래 속도로 복귀
+                   DOVirtual.DelayedCall(SLOW_DURATION, () =>
+                   {
+                       DOTween.To(() => Time.timeScale,
+                                  x => Time.timeScale = x,
+                                  1f,
+                                  TIME_RECOVER_DURATION);
+                   });
                });
     }
 }
