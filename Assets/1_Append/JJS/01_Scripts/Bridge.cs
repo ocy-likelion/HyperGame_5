d296a828c6690eb84acfc8adf1a51c135d87b5d2
[ESGUnity] <[sshsong3908@naver.com]>
2025-09-04 15:06:51 +0900

    feat : 광고 추가

--- Assets/1_Append/JJS/01_Scripts/Bridge.cs
+++ Assets/1_Append/JJS/01_Scripts/Bridge.cs
@@ -3,11 +3,10 @@ using UnityEngine;
 
 public class Bridge : MonoBehaviour
 {
-    #region 유니티에서 자바스크립트로
     [DllImport("__Internal")]
     private static extern void ExecuteJavaScriptMethod(string method); // 단순 메서드 실행용(반환값 없음)
-    //[DllImport("__Internal")]
-    //private static extern string ExecuteJavaScriptReturn(string method); // 반환값이 있는 메서드 실행용(단 무조건 string으로 반환됨)
+    [DllImport("__Internal")]
+    private static extern string ExecuteJavaScriptReturn(string method); // 반환값이 있는 메서드 실행용(단 무조건 string으로 반환됨)
 
     public static void OpenLeaderBoard()
     {
@@ -25,40 +24,38 @@ public class Bridge : MonoBehaviour
         Debug.Log("점수 제출");
 #endif
     }
-//    public static void LoadAd()
-//    {
-//#if UNITY_WEBGL && !UNITY_EDITOR
-//        ExecuteJavaScriptMethod($"loadInterstitialAd()");
-//#else
-//        Debug.Log("광고 불러오기");
-//#endif
-//    }
-//    public static void ShowAd()
-//    {
-//#if UNITY_WEBGL && !UNITY_EDITOR
-//        ExecuteJavaScriptMethod($"showInterstitialAd()");
-//#else
-//        Debug.Log("광고 보여주기");
-//#endif
-//    }
-//    public static AdLoadStatus GetAdStatus()
-//    {
-//#if UNITY_WEBGL && !UNITY_EDITOR
-//        string status = ExecuteJavaScriptReturn("GetAdLoadStatus()");
-//        switch (status)
-//        {
-//            case "loaded": return AdLoadStatus.Loaded;
-//            case "failed": return AdLoadStatus.Failed;
-//            case "closed": return AdLoadStatus.Closed;
-//            default: return AdLoadStatus.Not_Loaded;
-//        }
-//#else
-//        return AdLoadStatus.Not_Loaded;
-//#endif
-//    }
-    #endregion
+    public static void LoadAd()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        ExecuteJavaScriptMethod($"loadInterstitialAd()");
+#else
+        Debug.Log("광고 불러오기");
+#endif
+    }
+    public static void ShowAd()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        ExecuteJavaScriptMethod($"showInterstitialAd()");
+#else
+        Debug.Log("광고 보여주기");
+#endif
+    }
+    public static AdLoadStatus GetAdStatus()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        string status = ExecuteJavaScriptReturn("GetAdLoadStatus()");
+        switch (status)
+        {
+            case "loaded": return AdLoadStatus.Loaded;
+            case "failed": return AdLoadStatus.Failed;
+            case "closed": return AdLoadStatus.Closed;
+            default: return AdLoadStatus.Not_Loaded;
+        }
+#else
+        return AdLoadStatus.Not_Loaded;
+#endif
+    }
 
-    #region 자바스크립트에서 유니티로
     public void OnVisibilityChanged(string state)
     {
         // 백그라운드 실행 제한을 위한 코드
@@ -73,7 +70,6 @@ public class Bridge : MonoBehaviour
             Time.timeScale = 1f;
         }
     }
-    #endregion
 }
 public enum AdLoadStatus
 {
